5816b641202603d76d7ef63386df254d8bd4d3f4
Ph0enix-E <phoenixcedson@gmail.com>
2025-12-15 09:09:05 -0800

    Update KillCilantro.java

--- KillCilantro.java
+++ KillCilantro.java
@@ -1,26 +1,26 @@
 public class KillCilantro {
-public static String killCilantro(String string) {
-int i = 0;
-int x = 0;
-int y = 0;
-int  n = 0;
-String killCilantro = string;
-while (i + 7 < string.length()) {
-if (string.substring(i, i + 8).equals("cilantro")) {
-x = i;
-y = i + 8;
-killCilantro = killCilantro.substring(0, x - n) + "parsley" + killCilantro.substring(y - n);
-n += 1;
-}
-i += 1;
-}
-return killCilantro;
-}
+   public static String killCilantro(String string) {
+      int i = 0;
+      int x = 0;
+      int y = 0;
+      int  n = 0;
+      String killCilantro = string;
+      while (i + 7 < string.length()) {
+         if (string.substring(i, i + 8).equals("cilantro")) {
+            x = i;
+            y = i + 8;
+            killCilantro = killCilantro.substring(0, x - n) + "parsley" + killCilantro.substring(y - n);
+            n += 1;
+            }
+         i += 1;
+         }
+      return killCilantro;
+   }
 
 
 //I know the code is weird but it works
-public static void main(String[] args) {
-System.out.println(killCilantro("Acilantrobbcilantrobcdefgcilantrohijcilantrouuuucilantro"));
-System.out.println(killCilantro("cccilantrcilantrro"));
-}
+   public static void main(String[] args) {
+      System.out.println(killCilantro("Acilantrobbcilantrobcdefgcilantrohijcilantrouuuucilantro"));
+      System.out.println(killCilantro("cccilantrcilantrro"));
+   }
 }
